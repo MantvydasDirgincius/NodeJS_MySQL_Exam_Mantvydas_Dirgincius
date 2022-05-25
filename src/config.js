761af419f601187c868aea466dc283a5c66ea44a
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-05-25 17:01:52 +0300

    config file

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const PORT = process.env.PORT || 3000;
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+};
+
+module.exports = {
+  PORT,
+  dbConfig,
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('serveris veikia'));
